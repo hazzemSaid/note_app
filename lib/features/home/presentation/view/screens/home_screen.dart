21eaad80894541c1f919d6c531c218594f3bd011
haazemsaid <haazemsaidd@gmail.com>
2024-10-13 22:39:51 +0300

    Refactor home screen UI to add modal bottom sheet with rounded corners

--- lib/features/home/presentation/view/screens/home_screen.dart
+++ lib/features/home/presentation/view/screens/home_screen.dart
@@ -11,6 +11,9 @@ class HomeScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 context: context,
                 builder: (context) {
                   return const AddNode();
