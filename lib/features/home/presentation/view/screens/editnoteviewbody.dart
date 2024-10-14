1be7f423c3a7f441bf3295b0e6fb3ec87056d55d
haazemsaid <haazemsaidd@gmail.com>
2024-10-15 00:04:11 +0300

    Refactor home screen UI to add navigation to edit note view and improve input fields

--- lib/features/home/presentation/view/screens/editnoteviewbody.dart
+++ lib/features/home/presentation/view/screens/editnoteviewbody.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/CustomTextfield.dart';
+
 class Editnoteviewbody extends StatelessWidget {
   const Editnoteviewbody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        const SizedBox(
+          width: double.infinity,
+          height: 50,
+        ),
+        const CustomTextfield(
+          hint: 'title',
+          maxline: 1,
+        ),
+        const SizedBox(
+          width: double.infinity,
+          height: 30,
+        ),
+        const CustomTextfield(
+          maxline: 5,
+          hint: 'description',
+        ),
+        const SizedBox(
+          width: double.infinity,
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 45.0),
+          child: GestureDetector(
+              child: Container(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.cyan,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Center(
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )),
+        ),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Note_app extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Note App',
       theme: ThemeData.dark(),
       routerConfig: Routes.router,
