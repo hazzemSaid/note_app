5281afbdab8ece7446db11440a4a1b734edfbc39
haazemsaid <haazemsaidd@gmail.com>
2024-10-17 10:45:56 +0300

    Refactor main.dart to initialize Hive and open 'notes' box, and register NoteModelAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/core/routes/routes.dart';
+import 'package:note_app/model/note_model.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('notes');
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const Note_app());
 }
 
